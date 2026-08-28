0043ded918bbef07e9c9b3782289cca2e5f6443e
Jean-François Leblanc <jfleblanc@jfleblanc.com>
2026-08-28 14:11:25 -0400

    Update template 1.5.x

---
+++ src/scripts/Main.js
@@ -0,0 +1,14 @@
+/** Componsante Main de TimTools */
+export default class Main {
+  constructor(element) {
+    this.init();
+  }
+
+  /**
+   * Méthode d'initialisation
+   */
+  init() {
+    console.log("Nouvelle instance de la composante -> Main");
+  }
+}
+new Main();
